6a78c10dc2995429be2fe5c96f4c420d560361b1
Valentyn B. <valentyn.basiuk.work@gmail.com>
2026-03-27 23:45:09 +0200

    feat: configure JSON serialization to use string enums in API and Swagger

--- src/BulletinBoard.Api/Program.cs
+++ src/BulletinBoard.Api/Program.cs
@@ -1,15 +1,29 @@
-using BulletinBoard.Infrastructure;
 using BulletinBoard.Core;
+using BulletinBoard.Infrastructure;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddCore();
 builder.Services.AddInfrastructure(builder.Configuration);
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.Converters.Add(new System.Text.Json.Serialization.JsonStringEnumConverter());
+    });
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    var xmlFileApi = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPathApi = Path.Combine(AppContext.BaseDirectory, xmlFileApi);
+    if (File.Exists(xmlPathApi)) c.IncludeXmlComments(xmlPathApi);
+
+    var xmlFileCore = "BulletinBoard.Core.xml";
+    var xmlPathCore = Path.Combine(AppContext.BaseDirectory, xmlFileCore);
+    if (File.Exists(xmlPathCore)) c.IncludeXmlComments(xmlPathCore);
+});
 
 var app = builder.Build();
 
